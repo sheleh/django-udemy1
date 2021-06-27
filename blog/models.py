cf468a55b51ad8e30ed3a36262b79d33e67c6a71
sheleh <shelex87@gmail.com>
2021-06-27 16:10:54 +0300

    first commit

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField(default=timezone.now())
+
+    def __str__(self):
+        return self.title
